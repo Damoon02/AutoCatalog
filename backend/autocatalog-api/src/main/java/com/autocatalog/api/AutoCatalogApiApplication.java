1a38d1192352b9b0ce8536e085eb875d20a438b9
Damoon02 <alanlunab02@gmail.com>
2026-03-07 22:12:03 -0600

    Estructura inicial del proyecto

---
+++ backend/autocatalog-api/src/main/java/com/autocatalog/api/AutoCatalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.autocatalog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoCatalogApiApplication.class, args);
+	}
+
+}
---
+++ backend/autocatalog-api/src/test/java/com/autocatalog/api/AutoCatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autocatalog.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCatalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
